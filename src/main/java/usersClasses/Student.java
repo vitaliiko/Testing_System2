37de85621817128f1d1d1a3cc92551f4ab433e34
vitaliy.kobrin <vitaliy.kobrin@gmail.com>
2015-10-13 18:28:09 +0300

    separated general parameters of students and teachers in user abstract class

--- src/main/java/usersClasses/Student.java
+++ src/main/java/usersClasses/Student.java
@@ -1,64 +1,14 @@
 package usersClasses;
 
-import java.io.Serializable;
+public class Student extends User {
 
-public class Student implements Serializable {
-
-    private String surname;
-    private String name;
-    private String secondName;
-    private String userName;
     private int indexInGroup;
-    private char[] password;
     private StudentsGroup group;
 
     public Student(String surname, String name, String secondName, StudentsGroup group) {
-        this.surname = surname;
-        this.name = name;
-        this.secondName = secondName;
+        super(surname, name, secondName);
         this.group = group;
         indexInGroup = this.group.addStudent(this);
-        userName = surname + " " + name + " " + secondName;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public char[] getPassword() {
-        return password;
-    }
-
-    public void setPassword(char[] password) {
-        this.password = password;
     }
 
     public String getGroupName() {
@@ -71,21 +21,5 @@ public class Student implements Serializable {
 
     public int getIndexInGroup() {
         return indexInGroup;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Student student = (Student) o;
-
-        return userName.equals(student.userName);
-
-    }
-
-    @Override
-    public int hashCode() {
-        return userName.hashCode();
     }
 }
---
+++ src/main/java/usersClasses/User.java
@@ -0,0 +1,82 @@
+package usersClasses;
+
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+
+    private String surname;
+    private String name;
+    private String secondName;
+    private String userName;
+    private char[] password;
+
+    public User(String secondName, String surname, String name) {
+        this.secondName = secondName;
+        this.surname = surname;
+        this.name = name;
+        userName = surname + " " + name + " " + secondName;
+    }
+
+    public User(String surname, String name, String secondName, char[] password) {
+        this.surname = surname;
+        this.name = name;
+        this.secondName = secondName;
+        this.password = password;
+        userName = surname + " " + name + " " + secondName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return userName.equals(user.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return userName.hashCode();
+    }
+}
