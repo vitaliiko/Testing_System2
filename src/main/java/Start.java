e6599a1d7c478f7ccd9421bf34785d857214acac
vitaliy.kobrin <vitaliy.kobrin@gmail.com>
2015-10-19 12:07:08 +0300

    update teachers.ser, change main method structure

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,7 +1,6 @@
 import supporting.IOFileHandling;
 import testingClasses.Question;
 import testingClasses.TestTask;
-import userGI.AuthenticationGI;
 import userGI.ShowTaskGI;
 import usersClasses.Teacher;
 import usersClasses.TeacherController;
@@ -13,12 +12,12 @@ import java.util.Set;
 public class Start {
 
     public static void main(String[] args) {
-//        new ShowTaskGI();
 
 
 
-//        new AddQuestionGI(5);
+    }
 
+    public static void createWindow() {
         TeacherController controller = new TeacherController(IOFileHandling.loadTeachersSet());
         try {
 //            new AuthenticationGI(controller);
@@ -26,31 +25,34 @@ public class Start {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
-//        Set<Teacher> teacherSet = new HashSet<>();
-//        teacherSet.add(new Teacher("Іванов", "Іван", "Іванович", "111111"));
-//        teacherSet.add(new Teacher("Петров", "Іван", "Іванович", "111111"));
-//        teacherSet.add(new Teacher("Сидоров", "Іван", "Іванович", "111111"));
-//        teacherSet.add(new Teacher("Іваненко", "Іван", "Іванович", "111111"));
-//        teacherSet.add(new Teacher("Петренко", "Іван", "Іванович", "111111"));
-//        IOFileHandling.saveTeachersSet(teacherSet);
+    public static void initAnswersList() {
+        ArrayList<String> answersList = new ArrayList<>();
+        answersList.add("11111111111111111111111111111111111111111111");
+        answersList.add("2222222222222222222222222222222222222222222");
+        answersList.add("333333333333333333333333333333333333333333");
+        ArrayList<String> rightAnswersList = new ArrayList<>();
+        rightAnswersList.add("2222222222222222222222222222222222222222222");
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(new Question("dfdfdf", answersList, rightAnswersList));
+        questions.add(new Question("dfdf212121df", answersList, rightAnswersList));
+        questions.add(new Question("dfdfdf dfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
+        questions.add(new Question("dfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
+        questions.add(new Question("dfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
+        questions.add(new Question("dfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
+        TestTask testTask = new TestTask("111", "222", "333");
+        testTask.setQuestionsList(questions);
+        IOFileHandling.saveTestTask(testTask, "111");
+    }
 
-
-//        ArrayList<String> answersList = new ArrayList<>();
-//        answersList.add("11111111111111111111111111111111111111111111");
-//        answersList.add("2222222222222222222222222222222222222222222");
-//        answersList.add("333333333333333333333333333333333333333333");
-//        ArrayList<String> rightAnswersList = new ArrayList<>();
-//        rightAnswersList.add("2222222222222222222222222222222222222222222");
-//        ArrayList<Question> questions = new ArrayList<>();
-//        questions.add(new Question("dfdfdf", answersList, rightAnswersList));
-//        questions.add(new Question("dfdf212121df", answersList, rightAnswersList));
-//        questions.add(new Question("dfdfdf dfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
-//        questions.add(new Question("dfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
-//        questions.add(new Question("dfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
-//        questions.add(new Question("dfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdfdfdfdf dfdfdfdfdfdfdfdfdf dfdfdfdfdfdf dfdfdfdfdfdf dfdfdf dfdfdf dfdfdf", answersList, rightAnswersList));
-//        TestTask testTask = new TestTask("111", "222", "333");
-//        testTask.setQuestionsList(questions);
-//        IOFileHandling.saveTestTask(testTask, "111");
+    public static void initTeacherSet() {
+        Set<Teacher> teacherSet = new HashSet<>();
+        teacherSet.add(new Teacher("Іванов", "Іван", "Іванович", "111111"));
+        teacherSet.add(new Teacher("Петров", "Іван", "Іванович", "111111"));
+        teacherSet.add(new Teacher("Сидоров", "Іван", "Іванович", "111111"));
+        teacherSet.add(new Teacher("Іваненко", "Іван", "Іванович", "111111"));
+        teacherSet.add(new Teacher("Петренко", "Іван", "Іванович", "111111"));
+        IOFileHandling.saveTeachersSet(teacherSet);
     }
 }
